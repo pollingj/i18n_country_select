09f6a67b357375f953b2ef9e4a0b7c237c2ebd4b
John Polling <john@theled.co.uk>
2012-09-07 16:56:53 +0200

    Switching to Ruby 1.8.7 format

--- lib/i18n_country_select/form_builder.rb
+++ lib/i18n_country_select/form_builder.rb
@@ -1,7 +1,7 @@
 module I18nCountrySelect
 	module FormBuilder
 		def country_code_select(method, priority_countries = nil, options = {}, html_options = {})
-			@template.country_code_select(@object_name, method, priority_countries, options.merge(object: @object), html_options)
+			@template.country_code_select(@object_name, method, priority_countries, options.merge(:object => @object), html_options)
 		end
 	end
 end
